e0dc4d44fb9bcb0f9d1eb8fb086993490c4a95a3
Zubair Zia <54982998+zubairzia@users.noreply.github.com>
2025-11-25 19:29:14 +0500

    Update script.js

--- var_const_let_func/script.js
+++ var_const_let_func/script.js
@@ -6,3 +6,22 @@ console.log("The sum is", sum);
 const square = x => x * x;
 
 console.log("5² =", square(5));
+
+// Default Parameters
+// Default parameter function
+function Myfirst(name = "World") {
+    console.log("Hello",name); 
+}
+Myfirst();
+
+//Scope snapshot
+//scope snapshot function - This function shows how to define same value inside and outside of function
+let colour = "red";   // outer scope
+
+function paint() {
+  let colour = "blue";   // inner scope
+  console.log("inside:", colour);
+}
+
+paint();
+console.log("outside:", colour);
