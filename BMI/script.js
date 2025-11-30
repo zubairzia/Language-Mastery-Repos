110c2f505f669a68b06a662802b340cb34d23874
Zubair Zia <54982998+zubairzia@users.noreply.github.com>
2025-11-30 21:16:12 +0500

    Refactor BMI calculation with variable names

--- BMI/script.js
+++ BMI/script.js
@@ -1,17 +1,28 @@
+// name declaration
+const X_person = "Aslam Malik";
+const Y_person = "Wasim Gola";
+
 // masses in kg
-const Aslam_mass = 60;
-const Wasim_mass = 71;
+const X_mass = 60;
+const Y_mass = 71;
 
 // heights in metres (convert ft → m: 1 ft = 0.3048 m)
-const Aslam_height = 5.9 * 0.3048;   // ≈ 1.80 m
-const Wasim_height = 6.2 * 0.3048;   // ≈ 1.89 m
+const X_height = 5.9 * 0.3048;   // ≈ 1.80 m
+const Y_height = 6.2 * 0.3048;   // ≈ 1.89 m
 
 // BMI calculation
-const Aslam_BMI = Aslam_mass / (Aslam_height ** 2);
-const Wasim_BMI = Wasim_mass / (Wasim_height ** 2);
+const X_BMI = X_mass / (X_height ** 2);
+const Y_BMI = Y_mass / (Y_height ** 2);
 
-const markHigherBMI = Aslam_BMI > Wasim_BMI;
+const HigherBMI = X_BMI > Y_BMI;
 
-console.log(`Aslam's BMI: ${Aslam_BMI.toFixed(2)}`);
-console.log(`Wasim's BMI: ${Wasim_BMI.toFixed(2)}`);
-console.log(`Mark has higher BMI: ${markHigherBMI}`);
+if (X_BMI > Y_BMI) {
+    const msg = X_person+"'s BMI:"+X_BMI.toFixed(2)+" is Higher Than"+Y_person+"'s BMI:"+Y_BMI.toFixed(2);
+    console.log(msg);
+}
+else{
+    const msg = "Wasims's BMI:"+Y_BMI.toFixed(2)+" is Higher Than Aslam's BMI:"+X_BMI.toFixed(2);
+    console.log(msg);
+}
+console.log(`Aslam's BMI: ${X_BMI.toFixed(2)}`);
+console.log(`Wasim's BMI: ${Y_BMI.toFixed(2)}`)
