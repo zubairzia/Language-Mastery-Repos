b49047a1d3773380ff24e56def2272cc767d339f
Zubair Zia <54982998+zubairzia@users.noreply.github.com>
2025-12-04 22:16:51 +0500

    Add fruitProcessor function and juice logging

--- var_const_let_func/funcs.js
+++ var_const_let_func/funcs.js
@@ -3,3 +3,15 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
