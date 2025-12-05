f63d109f960795502568700e83d7ae2dffa3d55a
Zubair Zia <54982998+zubairzia@users.noreply.github.com>
2025-12-05 22:39:05 +0500

    Add function declaration and expression examples
    Added examples of function declarations and expressions.

--- var_const_let_func/funcs.js
+++ var_const_let_func/funcs.js
@@ -15,3 +15,19 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('24');
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
