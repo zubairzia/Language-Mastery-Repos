80ebce61342665a867ae8649978399f38422ae50
Zubair Zia <54982998+zubairzia@users.noreply.github.com>
2025-12-09 22:26:42 +0500

    Add showArgs function to log arguments

--- var_const_let_func/Regularfunc.js
+++ var_const_let_func/Regularfunc.js
@@ -4,3 +4,9 @@ function add(a,b){
     return a + b;
 }
 console.log(add(10,10));
+//////////////////////////////
+// Regular Function 2
+function showArgs() {
+  console.log(arguments);
+}
+showArgs(1, 2, 3);
